72bd38ba9e3d1f45b949e6a8a5059ec59ff460cf
손철훈 <sch9208@gmail.com>
2024-05-09 14:25:21 +0900

    데이터몬2 백엔드 초기 구축

---
+++ src/main/java/com/datamon/datamon2/Datamon2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.datamon.datamon2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datamon2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Datamon2BackendApplication.class, args);
+    }
+
+}
