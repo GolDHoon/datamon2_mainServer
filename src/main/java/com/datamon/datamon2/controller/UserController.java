d79552d21d29563514e869dcfd8a1fb6d1904a01
손철훈 <sch9208@gmail.com>
2024-05-21 19:18:40 +0900

    로그인 기능개발 - 오류 해소

--- src/main/java/com/datamon/datamon2/controller/UserController.java
+++ src/main/java/com/datamon/datamon2/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.datamon.datamon2.controller;
 
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableJdbcHttpSession
 @RestController
 public class UserController {
 
