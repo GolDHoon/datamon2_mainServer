40c43f77d85635221fff6bb89d13cc21477bff96
손철훈 <sch9208@gmail.com>
2024-05-23 13:13:58 +0900

    로그인 기능 오류 개선

--- src/main/java/com/datamon/datamon2/config/WebConfigurer.java
+++ src/main/java/com/datamon/datamon2/config/WebConfigurer.java
@@ -14,6 +14,9 @@ public class WebConfigurer implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000"
                                 , "http://158.247.215.85:3000")
+//                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
