76d1dda7f508ce8eeee9b4b67cbc528bc90aa326
손철훈 <sch9208@gmail.com>
2024-05-30 09:50:39 +0900

    랜딩페이지 아이피 테스트

--- src/main/java/com/datamon/datamon2/controller/LandingPageController.java
+++ src/main/java/com/datamon/datamon2/controller/LandingPageController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.Inet4Address;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.util.Enumeration;
 
 @EnableJdbcHttpSession
 @RestController
@@ -16,10 +16,20 @@ import java.net.UnknownHostException;
 public class LandingPageController {
 
     @GetMapping("/getIp")
-    public String getIp(HttpServletRequest request, HttpServletResponse response) throws UnknownHostException {
+    public String getIp(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String xfHeader = request.getHeader("X-Forwarded-For");
         String clientIp = request.getRemoteAddr();
         String hostAddress = Inet4Address.getLocalHost().getHostAddress();
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (networkInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = networkInterfaces.nextElement();
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            while (inetAddresses.hasMoreElements()) {
+                InetAddress inetAddress = inetAddresses.nextElement();
+                if (inetAddress.isSiteLocalAddress()) {
+                    System.out.println("내부 IP 주소: " + inetAddress.getHostAddress());
+                }
+            }
         System.out.println("백엔드 진입");
         return "S";
     }
