775a4c49b5641e6f2d511e0ab7be19fffab6b80d
손철훈 <sch9208@gmail.com>
2024-05-30 13:43:25 +0900

    랜딩페이지 아이피 테스트

--- src/main/java/com/datamon/datamon2/controller/LandingPageController.java
+++ src/main/java/com/datamon/datamon2/controller/LandingPageController.java
@@ -17,28 +17,6 @@ public class LandingPageController {
 
     @GetMapping("/getIp")
     public String getIp(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String xfHeader = request.getHeader("X-Forwarded-For");
-        String clientIp = request.getRemoteAddr();
-        String hostAddress = Inet4Address.getLocalHost().getHostAddress();
-
-        String hostAddress1 = InetAddress.getLocalHost().getHostAddress();
-        byte[] address = InetAddress.getLocalHost().getAddress();
-        System.out.println("백엔드 진입");
-
-
-        String hostAddress2 = "";
-
-        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-        while (networkInterfaces.hasMoreElements()) {
-            NetworkInterface networkInterface = networkInterfaces.nextElement();
-            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-            while (inetAddresses.hasMoreElements()) {
-                InetAddress inetAddress = inetAddresses.nextElement();
-                if (inetAddress instanceof Inet4Address && !inetAddress.isLoopbackAddress()) {
-                    hostAddress2 = inetAddress.getHostAddress();
-                }
-            }
-        }
-        return "S";
+        return request.getHeader("X-Forwarded-For");
     }
 }
