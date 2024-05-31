46f3db127cde400951d470338efde5a24cfdc943
손철훈 <sch9208@gmail.com>
2024-05-31 16:14:07 +0900

    cors 정책

--- src/main/java/com/datamon/datamon2/config/WebConfigurer.java
+++ src/main/java/com/datamon/datamon2/config/WebConfigurer.java
@@ -1,11 +1,16 @@
 package com.datamon.datamon2.config;
 
+import com.datamon.datamon2.common.CommonCodeCache;
+import com.datamon.datamon2.dto.repository.LpgeCodeDto;
 import com.datamon.datamon2.interceptor.PermissionCheckInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration		// 자동 호출되어 설정됨
 public class WebConfigurer implements WebMvcConfigurer {
@@ -15,12 +20,23 @@ public class WebConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
+        List<String> domainList = CommonCodeCache.getLpgeCodes().stream()
+                .filter(LpgeCodeDto::getUseYn)
+                .filter(dto -> !dto.getDelYn())
+                .map(LpgeCodeDto::getCodeValue)
+                .toList();
         // 모든 client 접속 허가
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000"
                                 , "http://localhost:63344/"
                                 , "https://datamon2.xyz")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+        registry.addMapping("/landingPage/**")
+                .allowedOrigins(domainList.toArray(new String[0]))
                 .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
