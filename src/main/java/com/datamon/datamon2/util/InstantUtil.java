900f4d0686aed9bc3615d08896372f446d50da7d
손철훈 <sch9208@gmail.com>
2024-06-05 17:15:25 +0900

    시간 버그 핫픽스

--- src/main/java/com/datamon/datamon2/util/InstantUtil.java
+++ src/main/java/com/datamon/datamon2/util/InstantUtil.java
@@ -6,10 +6,6 @@ import java.time.ZonedDateTime;
 
 public class InstantUtil {
     public Instant getNow(){
-        Instant now = Instant.now();
-        ZoneId korea = ZoneId.of("Asia/Seoul");
-        ZonedDateTime koreaZonedDateTime = now.atZone(korea);
-
-        return koreaZonedDateTime.toInstant();
+        return ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toInstant();
     }
 }
