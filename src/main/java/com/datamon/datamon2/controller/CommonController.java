9b43a70ac866d199f7dd5d4f0f7c1b67271f423b
손철훈 <sch9208@gmail.com>
2024-06-19 14:02:03 +0900

    랜딩페이지 고객 DB -> 범용 고객DB 설정

---
+++ src/main/java/com/datamon/datamon2/controller/CommonController.java
@@ -0,0 +1,37 @@
+package com.datamon.datamon2.controller;
+
+import com.datamon.datamon2.servcie.logic.CommonService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/common")
+public class CommonController {
+    private static final Logger logger = LogManager.getLogger(CommonController.class);
+    private CommonService commonService;
+
+    @GetMapping("/DBList")
+    public ResponseEntity<?> getDBList(HttpServletRequest request, HttpServletResponse response){
+        List<Map<String, String>> result;
+
+        try {
+            result = commonService.getDBListByUserIdForSession(request);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>("fail - serverEror", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+
+    }
+}
