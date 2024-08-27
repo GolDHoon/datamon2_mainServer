1a6f40cf42dc7ae25e78191421a11f14b1cd7c83
손철훈 <sch9208@gmail.com>
2024-08-27 10:02:59 +0900

    실적 기능 제작중

--- src/main/java/com/datamon/datamon2/controller/PerformanceController.java
+++ src/main/java/com/datamon/datamon2/controller/PerformanceController.java
@@ -1,7 +1,65 @@
 package com.datamon.datamon2.controller;
 
+import com.datamon.datamon2.servcie.logic.PerformanceService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController("/performance")
 public class PerformanceController {
+    private static final Logger logger = LogManager.getLogger(PerformanceController.class);
+    private PerformanceService performanceService;
+
+    public PerformanceController(PerformanceService performanceService) {
+        this.performanceService = performanceService;
+    }
+
+    @GetMapping("/ad/init")
+    public ResponseEntity<?> getAdPerformance (HttpServletRequest request, HttpServletResponse response){
+        Map<String, Object> result;
+        try {
+            result = performanceService.getAdPerformance(request);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>("fail - serverEror", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/crm/company/init")
+    public ResponseEntity<?> getCrmCompanyPerformance (HttpServletRequest request, HttpServletResponse response){
+        List<Map<String, Object>> result;
+        try {
+            result = performanceService.getCrmCompanyPerformance(request);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>("fail - serverEror", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/ad/member/init")
+    public ResponseEntity<?> getCrmMemberPerformance (HttpServletRequest request, HttpServletResponse response, @RequestParam String companyId){
+        Map<String, Object> result;
+        try {
+            result = performanceService.getCrmMemberPerformance(request, companyId);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>("fail - serverEror", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
